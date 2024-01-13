fee84f056effdd57bed562feff02e9f2c0c392f7
g610g <gio.gonzales@carsu.edu.ph>
2024-01-14 06:31:00 +0800

    traits practice

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,47 @@ mod customer {
         super::add_to_waitlist();
     }
 }
+pub mod authentication{
+    pub trait  Authenticatable{
+        fn display_user_credentials(&self);
+        fn username(&self)->&str;
+    }
+    #[derive(Debug)]
+    pub struct User{
+        username:String,
+        password:String,
+   }
+    impl Authenticatable for User{
+        fn display_user_credentials(&self){
+            println!("username: {}, password:{}", self.username, self.password);
+        }
+        fn username(&self)-> &str {
+            &self.username[..]
+        }
+    }
+    impl User{
+        pub fn new(username:String, password:String)->User{
+            User{
+                username,
+                password
+            }
+        }
+    }
+    pub fn verify_email<T: Authenticatable>(user1: &T){
+    }
+
+   pub struct Profile<T>{
+        user:T,
+        likes:u64,
+        has_badge:bool
+   }
+   impl <T: Authenticatable> Profile<T>{
+    pub fn show_profile(&self){
+        if self.has_badge{
+            println!("username:{} has {} likes and has a badge", self.user.username(), self.likes);
+        }else{
+            println!("user {} has no badge", self.user.username());
+        }
+    }
+   }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{self, Read};
 use std::fs::File;
+use std::cmp::{Ord,Ordering};
 use std::collections::HashMap;
 use rand::Rng;
+use practice_package::authentication::*;
 struct Guess{
    value:u32
 }
@@ -28,9 +30,15 @@ impl Guess{
 fn main() {
     let mut point_generic = Point{x:vec![1,2,3,4], y:1};
     let generic_field = point_generic.x();
+    let user = practice_package::authentication::User::new(String::from("g6i10"), String::from("password"));
+    practice_package::authentication::verify_email(&user);
+    user.display_user_credentials();
     println!("{:?}", point_generic.x);
     // println!("{}", );
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let a = 32;
+    let b = 1;
+    compare_two_int(a, &b);
     println!("{secret_number}");
     println!("Welcome to our guessing game program!");
     println!("Enter a guess between 1-100");
@@ -38,7 +46,7 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Error reading the stdin");
     let guess:u32 = guess.trim().parse().expect("Error parsing value");
     let guess_struct = Guess::new(guess);
-
+  
 
 }
 fn read_file() -> Result<String, io::Error>{
@@ -46,4 +54,11 @@ fn read_file() -> Result<String, io::Error>{
     let mut username = String::new();
     username_file_handle.read_to_string(&mut username)?;
     Ok(username)
+}
+fn compare_two_int(a:u32, b:&u32){
+    match a.cmp(b){
+        Ordering::Less => {println!("less")}
+        Ordering::Equal => {println!("Equal")}
+        Ordering::Greater => {println!("Greater")}
+    }
 }
