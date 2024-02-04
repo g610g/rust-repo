25eaa0fc264d627cae020f6a1ebd801adc0cc8fb
g610g <gio.gonzales@carsu.edu.ph>
2024-02-04 19:12:49 +0800

    OS exercise written in rust

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,56 @@
-use std::io::{BufReader,BufRead, Error };
-use std::process;
-use std::fs::{File};
+use std::{error::Error, io, process};
 #[derive(Debug)]
-struct Human{
-    name: String,
-    age:i32
+struct Process{
+    id:u32,
+    burst_time:u32,
+    turn_around_time:u32,
+    wait_time:u32
 }
-fn initialize_file(file_path: &str) -> Result<BufReader<File>, Error >{
-    let handler = File::open(file_path)?;
-    let buffer = BufReader::new(handler);
-    Ok(buffer)
+impl Process {
+    fn build (burst_time:u32, id:u32)-> Process {
+        Process{
+            id,
+            burst_time,
+            turn_around_time: 0,
+            wait_time:0 
+        }
+    }
+    fn assign_waiting_turn(&mut self, wait_time: u32){
+        self.wait_time = wait_time;
+        self.turn_around_time = self.wait_time + self.burst_time;
+
+    }
 }
-fn returns_option() -> Option<i32> {
-    None
+fn create_processes(length: u32, processes: &mut Vec<Process>) -> Result< &Vec<Process>, Box<dyn Error>> {
+    for i in 0..length{
+        let mut burst_time = String::new();
+        println!("Enter burst time for process {} : ", i);
+        io::stdin().read_line(&mut burst_time)?;        
+        let burst_time = burst_time.trim().parse::<u32>()?;
+        processes.push(Process::build(burst_time, i));
+    }
+    Ok(processes)
 }
-fn rename_humans(human_list: &Vec<Human>) -> Vec<Human> {
-    human_list.iter().map(|h|{
-        return Human{name:format!("{} Gonzales", h.name ), age:h.age + 1}
-    }).collect()
+fn  process_waiting_turn(processes: &mut Vec<Process>){
+    let mut  wait_time = 0;
+    processes.iter_mut().for_each(|p| {
+        p.assign_waiting_turn(wait_time);
+        wait_time += p.burst_time;
+    });
+
+    processes.iter().for_each(|p| println!("{:?}", p));
 }
-fn main() {
-    // let file_path = "assets/problem.txt";
-    // let file_buffer: BufReader<File>; 
-    // match initialize_file(file_path){
-    //     Ok(buffer) => file_buffer = buffer,
-    //     Err(err) => { 
-    //         println!("An error as occured: {}", err);
-    //         process::exit(1);
-    //     }
-    // }
-    // for line in file_buffer.lines() {
-    //     if let Ok(string_line) = line{
-    //     }
-    // }
-    let mut human_list = vec![
-        Human{name:"Gio".to_string(), age:22},
-        Human{name:"Mary".to_string(), age:15}
-    ];
-    let human_iter = human_list.iter(); 
-    
-    let modified_humans:Vec<Human> = human_iter.map(|h| {
-        return  Human{name:h.name.clone(), age:h.age + 1};
-    }).collect();
-    println!("{:?}", rename_humans(&human_list));
-    println!("{:?}", human_list);
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut process_length = String::new();
+    let mut processes:Vec<Process> = Vec::new();
+    println!("Enter number of processes: ");
+    io::stdin().read_line(&mut process_length)?;
+    let process_length = process_length.trim();
+    if let Err(e) = create_processes(process_length.parse()?, &mut processes){
+        println!("Error processing the program: {}", e);
+        process::exit(1);
+    }
+    process_waiting_turn(&mut processes);
+    Ok(())
+
 }
