0563ac31a0ca65a063d988169554a38b657e4c15
g610g <gio.gonzales@carsu.edu.ph>
2024-05-06 12:19:43 +0800

    refresh

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-use os_school::aoc;
 fn main() {
-    let res: &str;
-    let main_string = "mainstring";
-    res = bar(main_string);
-    println!("{res}");
-    aoc::say_hello();
-    aoc::day_one::testing();
-    // {
-    //     let my_ref = "Hello world";
-    //     let my_second_ref = "Hello another world";
-    //     res = foo(my_ref, my_second_ref);
-    // }
+    let returned = baz(Some(2)).unwrap();
+    println!("{}", returned);
 }
-fn foo<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        return x;
-    }
-    return y;
-}
-fn bar<'a>(foreign: &'a str) -> &'a str {
-    let local_string = "local string";
-    return foo(foreign, local_string);
+// fn foo<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         return x;
+//     }
+//     return y;
+// }
+// fn bar<'a>(foreign: &'a str) -> &'a str {
+//     let local_string = "local string";
+//     return foo(foreign, local_string);
+// }
+fn baz(opts: Option<usize>) -> Option<usize> {
+    return opts.map(|x| x * 5);
 }
